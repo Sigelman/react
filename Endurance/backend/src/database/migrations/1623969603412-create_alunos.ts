c0960ea8674c5e355c4bc14c3225aaf2a0b5bd0e
Marco <m.a.romano@terra.com.br>
2021-06-27 21:19:56 -0300

    correções nas migrations

--- Endurance/backend/src/database/migrations/1623969603412-create_alunos.ts
+++ Endurance/backend/src/database/migrations/1623969603412-create_alunos.ts
@@ -1,5 +1,6 @@
 import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
 
+
 export class createAlunos1623969603412 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
