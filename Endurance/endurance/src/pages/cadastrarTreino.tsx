5c877aca4a381d6f34c2572f1f94fde7fc28fb4d
sigel <sigelman.araujo@gmail.com>
2021-07-05 15:17:51 -0300

    alterações cadastros de treinos

--- Endurance/endurance/src/pages/cadastrarTreino.tsx
+++ Endurance/endurance/src/pages/cadastrarTreino.tsx
@@ -5,94 +5,70 @@ import { FiArrowLeft } from "react-icons/fi";
 import CadloginImg from '../images/fitness_homemMulher.png';
 import api from '../services/api';
 
-import '../styles/pages/cadastrarAluno.css';
+import '../styles/pages/cadastrarTreino.css';
 
-interface alunoX  {
+interface Treino  {
   //id:string;
-  nome:string;
-  idade:string;
-  sexo:string;
-  peso:string;
-  altura:string;
-  cintura:string;
-  sessoes:string;
-  personal_id:string;
+  metodo:number;
+  minutagem:number;
+  distancia: number;
+  zonaesforco:number;
+  terreno:number;
+  pseprevista:number;
+  sessao:number;
+  aluno_id:number;
 }
 
+interface EnumServiceItem {
+  id: number; label: string; 
+}
+
+//interface EnumServiceItems extends Array<EnumServiceItem>{}
 
 function CadastrarTreino() {
+  const history = useHistory();
+  const queryParams = new URLSearchParams(history.location.search);
+  const sessoes = Number(queryParams.get('sessoes'));
+  const [options, setOptions] = useState<EnumServiceItem[]>([]);
   
+  useEffect(()=> {
+    let indents = options;
+      for (let i = 0; i < sessoes; i++) {
+        indents.push({"id":i+1,"label":"Sessão "+(i+1)});
+      };
+      setOptions(indents);
+    },[]);
 
-    const history = useHistory();
-    const queryParams = new URLSearchParams(history.location.search);
+
   
-    const [nome,setNome] = useState('');
-    const [idade,setIdade] = useState('');
-    const [sexo,setSexo] = useState('');
-    const [peso,setPeso] = useState('');
-    const [altura,setAltura] = useState('');
-    const [cintura,setCintura] = useState('');
-    const [sessoes,setSessoes] = useState('2');
-    const [personal_id,setPersonalId] = useState('');
-    const [personalNome,setPersonalNome] = useState('');
-
-    //const [alunoAtualizado, setAlunoAtualizado] = useState<alunoX>();
-
-
-    useEffect(()=> {
-    //api.get('alunos').then(response => {
-    //  setAlunos(response.data);
-    //  });
-    fetchAluno();
-        },[]);
-
-    async function fetchAluno()  {
-        const response = await api.get('alunos/'+queryParams.get('id'));
-        
-        const  {nome,idade,sexo,peso,altura,cintura,sessoes,personal_id} = response.data;
-        const aluno = {nome,idade,sexo,peso,altura,cintura,sessoes,personal_id};
-            
-        //const [aluno,setAluno] = useState<Alunos>(response.data);
-        setNome(aluno.nome);
-        setIdade(aluno.idade);
-        setSexo(aluno.sexo);
-        setPeso(aluno.peso);
-        setAltura(aluno.altura);
-        setCintura(aluno.cintura);
-        setSessoes(aluno.sessoes);
-        setPersonalId(aluno.personal_id);
-        
-        const responsePersonal = await api.get('personals/'+personal_id);
-
-        const personal = responsePersonal.data;
-        setPersonalNome(personal.nome); 
-
-       }
-
+    const [sessao,setSessao] = useState('1');
+    const [metodo, setMetodo] = useState('1');
+    const [minutagem,setMinutagem] = useState('');
+    const [distancia,setDistancia] = useState('');
+    const [zonaesforco,setZonaesforco] = useState('1');
+    const [terreno,setTerreno] = useState('1');
+    const [pseprevista,setPseprevista] = useState('1');
+    const [aluno_id,setIdaluno] = useState(queryParams.get('id'));
+    const [cargaprevista,setCargaprevista] = useState((Number(minutagem)*Number(pseprevista)).toString());
+  
     async function handleSubmit(event: FormEvent){
       event.preventDefault();
 
-      const alunoAtualizado = {nome,idade,sexo,peso,altura,cintura,sessoes,personal_id};
-
-      console.log(alunoAtualizado);
-
-      const response = await api.put('alunos/'+queryParams.get('id'), alunoAtualizado);
+      const cadastroTreino = {metodo,minutagem,distancia,zonaesforco,terreno,pseprevista,sessao,aluno_id};
+      const response = await api.post('treinos/', cadastroTreino);
       alert('cadastro realizado com suesso');
-      history.push('/aluno');
-
-      //await api.put('alunos',aluno);
-      //alert('cadastro realizado com suesso');
-        
+      //history.push('/treino');
+       
   }; 
 
    const { goBack } = useHistory();
 
     return (
-        <div id="page-cadastrarAluno">
+        <div id="page-cadastrarTreino">
             <aside>
                <header>
                    <img src={CadloginImg} alt="login" />
-                   <h2>Atualizar Cadastro dos alunos</h2>
+                   <h2>Cadastrar Treinos</h2>
                    <p>Sua saúde em primeiro lugar :)</p>
                </header> 
                <footer>
@@ -104,72 +80,71 @@ function CadastrarTreino() {
                </footer>
             </aside>
             <main>
-                <form onSubmit={handleSubmit} className="create-cadaluno-form">
-                    <div className="input-block">
-                      <label htmlFor="name">Nome</label>
-                      <input id="nome" value={nome} onChange={event =>setNome(event.target.value)}/>
+                <form onSubmit={handleSubmit} className="create-cadtreino-form">
+                      <div className="input-block">
+                      <label htmlFor="name">Método</label>
+                      <select id="metodo" value={metodo} onChange={event =>setMetodo(event.target.value)}>
+                           <option value='1'>Contínuo</option>
+                           <option value='2'>Intervalado</option>
+                           <option value='3'>Fartlek</option>
+                           <option value='4'>Contínuo Variativo</option>
+                      </select>                      
                     </div>
                     <div className="input-block">
-                      <label htmlFor="name">Idade</label>
-                      <input id="idade" value={idade} onChange={event =>setIdade(event.target.value)}/>
+                      <label htmlFor="name">Minutagem</label>
+                      <input id="minutagem" value={minutagem} onChange={event =>setMinutagem(event.target.value)}/>
                     </div>
                     <div className="input-block">
-                      <label htmlFor="name">Sexo</label>
-                      <input id="sexo" value={sexo} onChange={event =>setSexo(event.target.value)}/>
+                      <label htmlFor="name">Distância</label>
+                      <input id="distancia" value={distancia} onChange={event =>setDistancia(event.target.value)}/>
                     </div>
                     <div className="input-block">
-                      <label htmlFor="name">Peso</label>
-                      <input id="peso" value={peso} onChange={event =>setPeso(event.target.value)}/>
+                      <label htmlFor="name">Zona de esforço</label>
+                      <select id="zonaesforco" value={zonaesforco} onChange={event =>setZonaesforco(event.target.value)}>
+                           <option value='1'>Leve</option>
+                           <option value='2'>Moderado</option>
+                           <option value='3'>Vigoroso</option>
+                      </select>
+                    </div>
+                    <div className="input-block"> 
+                      <label htmlFor="name">Terreno</label>
+                      <select id="terreno" value={terreno} onChange={event =>setTerreno(event.target.value)}>
+                           <option value='1'>Asfalto</option>
+                           <option value='2'>Pista</option>
+                           <option value='3'>Piçarra</option>
+                           <option value='3'>Trilha</option>
+                           <option value='3'>Misto</option>
+                      </select>
                     </div>
                     <div className="input-block">
-                      <label htmlFor="name">Altura</label>
-                      <input id="altura" value={altura} onChange={event =>setAltura(event.target.value)}/>
+                      <label htmlFor="name">PSE Prevista</label>
+                      <select id="pseprevista" value={pseprevista} onChange={event =>setPseprevista(event.target.value)}>
+                           <option value='1'>Um</option>
+                           <option value='2'>Dois</option>
+                           <option value='3'>Três</option>
+                           <option value='4'>Quatro</option>
+                           <option value='5'>Cinco</option>
+                           <option value='6'>Seis</option>
+                           <option value='7'>Sete</option>
+                           <option value='8'>Oito</option>
+                           <option value='9'>Nove</option>
+                           <option value='10'>Dez</option>
+                      </select>
                     </div>
-                    <div className="input-block">
-                      <label htmlFor="name">Cintura</label>
-                      <input id="Cintura" value={cintura} onChange={event =>setCintura(event.target.value)}/>
-                    </div>
-                    <div className="sessoes"><label>Sessoes</label></div>
-                    <div className="radio">
-                      <label>
-                        <input
-                          type="radio" name="Nsessoes"
-                          value="2" onChange={event =>setSessoes(event.target.value)}  checked={sessoes=="2"}
-                        />
-                        Duas
-                      </label>
-                    </div>
-                    <div className="radio">
-                      <label>
-                        <input
-                          type="radio" name="Nsessoes"
-                          value="3" onChange={event =>setSessoes(event.target.value)} checked={sessoes=="3"}
-                        />
-                        Três
-                      </label>
-                    </div>
-                    <div className="radio">
-                      <label>
-                        <input
-                          type="radio" name="Nsessoes"
-                          value="4" onChange={event =>setSessoes(event.target.value)} checked={sessoes=="4"}
-                        />
-                        Quatro
-                      </label>
-                    </div>                    
-                    <div className="radio">
-                      <label>
-                        <input
-                          type="radio" name="Nsessoes"
-                          value="5" onChange={event =>setSessoes(event.target.value)} checked={sessoes=="5"}
-                        />
-                        Cinco
-                      </label>
-                    </div>    
                                    
                     <div className="input-block">
-                      <label htmlFor="name">Personal</label>
-                      <input id="personal" value={personalNome} onChange={event =>setPersonalNome(event.target.value)} readOnly={true}/>
+                      <label htmlFor="name">Carga Prevista</label>
+                      <input id="cargaprevista" value={cargaprevista} onChange={event =>setCargaprevista(event.target.value)} readOnly={true}/>
+                    </div>
+
+                    <div className="input-block">
+                      <label htmlFor="name">Sessão</label>
+                      <select id="sessao" value={sessao} onChange={event =>setSessao(event.target.value)}>
+                        {options.map((option: { id: {} | null | undefined; })=>(
+                           <option key={option.id} value={option.id}>Sessão{option.id}</option>
+                        )
+                        )}
+                      </select>
                     </div>
 
                     <button className="confirm-button" type="submit">
